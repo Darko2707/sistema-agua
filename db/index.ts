3d0ad8e75b3ec340f345d272cb96a9756eb194f9
Darko2707 <reyesronaldo446@gmail.com>
2026-06-14 09:16:02 -0600

    db index

--- db/index.ts
+++ db/index.ts
@@ -1,17 +1,7 @@
-import { neon } from '@neondatabase/serverless'
-import { drizzle } from 'drizzle-orm/neon-http'
+import { Pool } from '@neondatabase/serverless'
+import { drizzle } from 'drizzle-orm/neon-serverless'
 import * as schema from './schema'
 
-const createDb = () => {
-  const url = process.env.DATABASE_URL!
-  return drizzle(neon(url), { schema })
-}
+const pool = new Pool({ connectionString: process.env.DATABASE_URL! })
 
-let _db: ReturnType<typeof createDb> | null = null
-
-export const db = new Proxy({} as ReturnType<typeof createDb>, {
-  get(_, prop) {
-    if (!_db) _db = createDb()
-    return (_db as any)[prop]
-  }
-})
+export const db = drizzle(pool, { schema })
